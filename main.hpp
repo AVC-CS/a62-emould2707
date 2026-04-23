0588d6e7d071f9416ede273e9128d52df04ca94b
emould2707 <emould@students.avc.edu>
2026-04-23 16:22:50 -0700

    a62 done i believe

--- main.hpp
+++ main.hpp
@@ -21,12 +21,12 @@ int getClosestMed(int rdnum1, int rdnum2, int rdnum3)
     // TODO
     double mean = (rdnum1 + rdnum2 + rdnum3) / 3.0;
     int closest = rdnum1;
-    double minGap = abs(rdnum1 - mean);
-    if (abs(rdnum2 - mean) < minGap) {
+    double minGap = fabs(rdnum1 - mean);
+    if (fabs(rdnum2 - mean) < minGap) {
         closest = rdnum2;
-        minGap = abs(rdnum2 - mean);
+        minGap = fabs(rdnum2 - mean);
     }
-    if (abs(rdnum3 - mean) < minGap) {
+    if (fabs(rdnum3 - mean) < minGap) {
         closest = rdnum3;
     }
     return closest;
